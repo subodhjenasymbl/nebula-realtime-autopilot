0f4dac405f43fbbf1c6ff9ff3c501e8c23bc4ab6
Subodh Jena <subodh.jena@symbl.ai>
2024-03-13 10:41:59 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Nebula Realtime AutoPilot
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nebula_realtime_autopilot",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A small example package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/rammerai/nebula_realtime_autopilot",
+)
